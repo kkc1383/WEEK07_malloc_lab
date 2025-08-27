09d86ffeee61090548a70d49f94653ae950990e2
kkc1383 <kangkc09@gmail.com>
2025-08-27 15:38:19 +0900

    일단 addfreeblock함수를 주소랑 LIFO모두 만들었고, find도 best_fit이 되어있음

--- malloc-lab/mm.c
+++ malloc-lab/mm.c
@@ -567,31 +567,37 @@ static void addFreeBlock(void* bp){
         fl_head=bp; // newptr을 헤드로 임명
     }
     else{ //자기 주소를 찾아가야함.
-        if(bp<=fl_head){ // 주소 맨 앞에 와야할 경우
-            PUT_PREV(bp,NULL); // 맨 앞에 올 노드니까 이전은 없고
-            PUT_NEXT(bp,fl_head); // 다음 노드가 원래 헤드였던 노드
-            PUT_PREV(fl_head,bp); // 그리고 원래 헤드였던 노드의 이전이 추가한 블락이고
-            fl_head=bp;// 추가한 블락을 헤드로 임명
-        }
-        else{
-            void* iter;
-            void* prev;
-            for(iter=fl_head;iter!=NULL;iter=GET_NEXT(iter)){
-                prev=iter;
-                if(GET_NEXT(iter)!=NULL && bp<GET_NEXT(iter)){ //나보다 큰 주소를 만났으면, 그게 iter의 next, iter과 iternext 사이에 들어오면 됨
-                    PUT_PREV(bp,iter);
-                    PUT_NEXT(bp,GET_NEXT(iter));
-                    PUT_PREV(GET_NEXT(iter),bp);
-                    PUT_NEXT(iter,bp);
-                    return;
-                }
-            }
-            //맨 끝에 와야하는 경우
-            PUT_PREV(bp,prev);
-            PUT_NEXT(bp,NULL);
-            PUT_NEXT(prev,bp);
-            return;
-        }
+        //LIFO 방식
+        PUT_NEXT(bp,fl_head);
+        PUT_PREV(fl_head,bp);
+        PUT_PREV(bp,NULL);
+        fl_head=bp;
+        // 주소순서 방식
+        // if(bp<=fl_head){ // 주소 맨 앞에 와야할 경우
+        //     PUT_PREV(bp,NULL); // 맨 앞에 올 노드니까 이전은 없고
+        //     PUT_NEXT(bp,fl_head); // 다음 노드가 원래 헤드였던 노드
+        //     PUT_PREV(fl_head,bp); // 그리고 원래 헤드였던 노드의 이전이 추가한 블락이고
+        //     fl_head=bp;// 추가한 블락을 헤드로 임명
+        // }
+        // else{
+        //     void* iter;
+        //     void* prev;
+        //     for(iter=fl_head;iter!=NULL;iter=GET_NEXT(iter)){
+        //         prev=iter;
+        //         if(GET_NEXT(iter)!=NULL && bp<GET_NEXT(iter)){ //나보다 큰 주소를 만났으면, 그게 iter의 next, iter과 iternext 사이에 들어오면 됨
+        //             PUT_PREV(bp,iter);
+        //             PUT_NEXT(bp,GET_NEXT(iter));
+        //             PUT_PREV(GET_NEXT(iter),bp);
+        //             PUT_NEXT(iter,bp);
+        //             return;
+        //         }
+        //     }
+        //     //맨 끝에 와야하는 경우
+        //     PUT_PREV(bp,prev);
+        //     PUT_NEXT(bp,NULL);
+        //     PUT_NEXT(prev,bp);
+        //     return;
+        // }
     }
 }
 
